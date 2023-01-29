cecc79d706addf30e6bce134b9359f6e95e18ef5
Mikołaj Siarkowski <mikolaj.siarkowski@gmail.com>
2023-01-29 13:10:20 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { WorldSVG } from './views/WorldSVG/WorldSVG';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navBar">
+        <ul className="navItems">
+          <li>SVG</li>
+          <li>Google</li>
+          <li>About</li>
+        </ul>
+      </div>
+      <h1>
+        World of songs
+      </h1>
+      <WorldSVG />
     </div>
   );
 }
